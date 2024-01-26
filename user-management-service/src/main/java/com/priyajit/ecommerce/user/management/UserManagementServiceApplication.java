2014ab9b7ed79d095a9438fcb8263e73a94875cb
Priyajit Bera <priyajitbera@gmail.com>
2024-01-26 22:46:20 +0530

    Cleanup

--- user-management-service/src/main/java/com/priyajit/ecommerce/user/management/UserManagementServiceApplication.java
+++ user-management-service/src/main/java/com/priyajit/ecommerce/user/management/UserManagementServiceApplication.java
@@ -1,16 +1,11 @@
 package com.priyajit.ecommerce.user.management;
 
-import com.priyajit.ecommerce.user.management.entity.DbEnvironmentConfiguration;
-import com.priyajit.ecommerce.user.management.entity.DbEnvironmentConfigurationProperty;
 import com.priyajit.ecommerce.user.management.repository.DbEnvironmentConfigurationRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
-public class UserManagementServiceApplication{
+public class UserManagementServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementServiceApplication.class, args);
