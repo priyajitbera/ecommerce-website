6984b2f0c92ae4a27795fb3d90eca42dfa03b03a
Priyajit Bera <priyajitbera@gmail.com>
2024-04-21 12:42:27 +0530

    Add ResponseEnity as wrapper for API response models

--- product-catalog-service/src/main/java/com/priyajit/ecommerce/product/catalog/service/controller/ProductImageControllerV1.java
+++ product-catalog-service/src/main/java/com/priyajit/ecommerce/product/catalog/service/controller/ProductImageControllerV1.java
@@ -1,11 +1,18 @@
 package com.priyajit.ecommerce.product.catalog.service.controller;
 
 import com.priyajit.ecommerce.product.catalog.service.dto.UploadProductImagesDto;
+import com.priyajit.ecommerce.product.catalog.service.model.Response;
 import com.priyajit.ecommerce.product.catalog.service.model.UploadProductImagesModel;
 import com.priyajit.ecommerce.product.catalog.service.service.ProductImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/product-image/v1")
@@ -15,9 +22,25 @@ public class ProductImageControllerV1 {
     private ProductImageService productImageService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public UploadProductImagesModel uploadProductImages(
+    public ResponseEntity<Response<UploadProductImagesModel>> uploadProductImages(
             @ModelAttribute UploadProductImagesDto dto
     ) {
-        return productImageService.uploadProductImages(dto);
+        try {
+            var model = productImageService.uploadProductImages(dto);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(Response.<UploadProductImagesModel>builder()
+                            .data(model)
+                            .build());
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode())
+                    .body(Response.<UploadProductImagesModel>builder()
+                            .error(e.getMessage())
+                            .build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Response.<UploadProductImagesModel>builder()
+                            .error(e.getMessage())
+                            .build());
+        }
     }
 }
