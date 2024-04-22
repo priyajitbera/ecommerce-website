f4d57514e3199711e4d876469c12667e8e280975
Priyajit Bera <priyajitbera@gmail.com>
2024-04-22 17:51:56 +0530

    Enable CORS all origins

--- user-management-service/src/main/java/com/priyajit/ecommerce/user/management/controller/AuthControllerV1.java
+++ user-management-service/src/main/java/com/priyajit/ecommerce/user/management/controller/AuthControllerV1.java
@@ -3,13 +3,11 @@ package com.priyajit.ecommerce.user.management.controller;
 import com.priyajit.ecommerce.user.management.dto.LoginDto;
 import com.priyajit.ecommerce.user.management.model.LoginModel;
 import com.priyajit.ecommerce.user.management.service.service.LoginService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/auth")
+@CrossOrigin(originPatterns = "*")
 public class AuthControllerV1 {
 
     private LoginService loginService;
