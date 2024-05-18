b9c27136a44401f281619a9c8f696304d331f7b6
Priyajit Bera <priyajitbera@gmail.com>
2024-05-18 23:00:13 +0530

    Fix NullPointerException when null argument passed, update getting Method from JointPoint

--- product-catalog-service/src/main/java/com/priyajit/ecommerce/product/catalog/service/validator/ConstraintViolationExceptionAspect.java
+++ product-catalog-service/src/main/java/com/priyajit/ecommerce/product/catalog/service/validator/ConstraintViolationExceptionAspect.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -28,12 +29,8 @@ public class ConstraintViolationExceptionAspect {
     @Before("execution(* com.priyajit.ecommerce.product.catalog.service.service.*.*.*(..))")
     void validate(JoinPoint jp) throws NoSuchMethodException {
 
-        String methodName = jp.getSignature().getName();
-        Class<?>[] classArr = new Class[jp.getArgs().length];
-        for (int i = 0; i < jp.getArgs().length; i++) {
-            classArr[i] = jp.getArgs()[i].getClass();
-        }
-        Method method = jp.getTarget().getClass().getMethod(methodName, classArr);
+        MethodSignature methodSignature = (MethodSignature) jp.getSignature();
+        Method method = methodSignature.getMethod();
         Parameter[] parameters = method.getParameters();
 
         for (int i = 0; i < parameters.length; i++) {
