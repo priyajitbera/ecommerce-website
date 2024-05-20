114b0ee09a4826f8e17b4074c1bed2b00404d69a
Priyajit Bera <priyajitbera@gmail.com>
2024-05-20 20:48:26 +0530

    Improve validation with annotation based declarative approach

---
+++ cart-service/src/main/java/com/priyajit/ecommerce/cart/service/validator/ConstraintViolationValidator.java
@@ -0,0 +1,23 @@
+package com.priyajit.ecommerce.cart.service.validator;
+
+import com.priyajit.ecommerce.cart.service.exception.CustomConstraintViolationException;
+import jakarta.validation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class ConstraintViolationValidator<T> {
+
+    void validate(T object) throws ConstraintViolationException {
+
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(object);
+        for (var violation : violations) {
+            System.out.println(violation);
+            String message = violation.getPropertyPath() + " " + violation.getMessage();
+            throw new CustomConstraintViolationException(message);
+        }
+    }
+}
