e7c550b1d4d6d342f87185cbccbe705f38663c4a
Priyajit Bera <priyajitbera@gmail.com>
2024-07-27 23:04:49 +0530

    Clean: remove unused imports

--- ecommerce-facade-service/src/main/java/com/priyajit/ecommerce/facade/service/EcommerceFacadeServiceApplication.java
+++ ecommerce-facade-service/src/main/java/com/priyajit/ecommerce/facade/service/EcommerceFacadeServiceApplication.java
@@ -1,7 +1,6 @@
 package com.priyajit.ecommerce.facade.service;
 
 import com.priyajit.ecommerce.facade.service.config.properties.*;
-import com.priyajit.ecommerce.facadeservice.config.properties.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
