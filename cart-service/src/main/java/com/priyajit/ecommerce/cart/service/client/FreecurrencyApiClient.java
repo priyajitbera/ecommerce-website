a100ddb481539e494601503e43712a153af5bd34
Priyajit Bera <priyajitbera@gmail.com>
2024-10-18 01:08:40 +0530

    delete unused api client files

--- cart-service/src/main/java/com/priyajit/ecommerce/cart/service/client/FreecurrencyApiClient.java
+++
@@ -1,8 +0,0 @@
-package com.priyajit.ecommerce.cart.service.client;
-
-import com.priyajit.ecommerce.cart.service.client.model.FreecurrencyApiModel;
-
-public interface FreecurrencyApiClient {
-
-    FreecurrencyApiModel getExchangeRate(String baseCurrency);
-}
